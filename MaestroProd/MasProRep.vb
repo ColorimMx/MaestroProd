41b8b462789d23cfeeb9f15f481707bb810bbad1
Pablo Colorim <pablo.amador@colorim.com.mx>
2020-01-27 21:01:33 -0600

    Versión 13.0.26 Crystal Runtime
    Actualización de la versión 13.0.26 de Crystal Reports Runtime.

---
+++ MaestroProd/MasProRep.vb
@@ -0,0 +1,9 @@
+﻿Public Class MasProRep
+
+    Private Sub CrystalReportViewer1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CrystalReportViewer1.Load
+        Dim MasPro As New MasterProd
+        Me.CrystalReportViewer1.ReportSource = MasPro
+        Me.CrystalReportViewer1.LogOnInfo.Item(0).ConnectionInfo.UserID = "ODBC"
+        Me.CrystalReportViewer1.LogOnInfo.Item(0).ConnectionInfo.Password = "ODBC"
+    End Sub
+End Class
